ead3eb335d7a4e940acda953aea4b4dc9bb8f2af
Moksha Upadhyaya <mokshaupadhyaya@gmail.com>
2024-10-09 00:49:27 +0530

    added some new commands from lecture 6

---
+++ initial_commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install nasm # used to install nasm
+sudo apt install qemu-system-x86 # used to install qemu emulator
+qemu-system-x86_64 # used to run qemu emulator
+nasm -f bin ./boot.asm -o ./boot.bin # used to convert .asm file to binary file.
+ndisasm ./boot.bin # this is a deassembler which is used to convert binary file to assembly level instruction.
+qemu-system-x86_64 -hda ./boot.bin # this command is used to run our boot loader in qemu emulator.
