b3ae1a78c398df3447fcfbea2fdeb688f4aad08e
Moksha Upadhyaya <mokshaupadhyaya@gmail.com>
2026-05-16 23:34:26 +0530

    Read data from disk as well as created a new folder for protected mode

--- real_mode/boot.asm
+++ real_mode/boot.asm
@@ -9,12 +9,6 @@ times 33 db 0
 start:
     jmp 0x7c0:step2
 
-handle_zero:
-    mov ah,0eh
-    mov bx,0x00
-    mov al,'A'
-    int 0x10
-    iret
 step2:
     cli ; clear interrupts to prevent any issues during boot
     mov ax, 0x7c0 ; set up the segment registers
@@ -24,13 +18,19 @@ step2:
     mov ss, ax
     mov sp, 0x7c00 ; set up the stack
     sti ; enable interrupts after setup
-    mov word[ss:0x00],handle_zero ; set the interrupt handler for interrupt 0
-    mov word[ss:0x02],0x7c0 ; set the code segment for the interrupt handler
-
-    mov ax,0x00
-    div ax
-
-    mov si, message
+    mov ah,02h
+    mov al,1
+    mov ch,0
+    mov cl,2
+    mov dh,0
+    mov bx,buffer
+    int 0x13 ; BIOS interrupt to read from disk
+    jc .disk_error ; if carry flag is set, there was an error
+    mov si, buffer ; point SI to the buffer where the message is stored
+    call print ; call the print function to display the message
+    jmp $ ; infinite loop to prevent the system from doing anything else
+.disk_error:
+    mov si, disk_error_msg
     call print
     jmp $
 print:
@@ -48,6 +48,7 @@ print_char:
     mov ah, 0eh;
     int 0x10
     ret
-    message db 'Hello, World!',0    
-    times 510 - ($ - $$) db 0
-    dw 0xAA55
+disk_error_msg db 'Not able to read disk', 0
+times 510 - ($ - $$) db 0
+dw 0xAA55
+buffer:
